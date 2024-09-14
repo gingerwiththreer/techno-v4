79c61b585c14d362122edd48c73ff160de3f72f5
Samrat Choudhary <samrat.choudhary757@gmail.com>
2024-09-14 18:10:20 +0530

    hnn

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,4 @@
-/** @type {import('next').NextConfig} */ 
-const nextConfig = {
-    output: 'export',
-    reactStrictMode: true,
-    }
-module.exports = nextConfig
-    
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default nextConfig;
