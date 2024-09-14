fb8cc0ec1f31eb85fbf31035899d11ecd727492d
Samrat Choudhary <samrat.choudhary757@gmail.com>
2024-09-14 18:04:58 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+/** @type {import('next').NextConfig} */ 
+const nextConfig = {
+    output: 'export',
+    reactStrictMode: true,
+    }
+module.exports = nextConfig
+    
